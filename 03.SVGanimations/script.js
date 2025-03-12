ba72721a694140cadac4a55742246335dc1efff1
hemant <tyagi26326@gmail.com>
2025-03-12 21:26:13 +0530

    WIP

---
+++ 03.SVGanimations/script.js
@@ -0,0 +1,28 @@
+console.log("hello world");
+
+var initialPath = `M 200 500 Q 850 500 1700 500`
+var finalPath = `M 200 500 Q 850 500 1700 500`
+
+var string = document.querySelector(".string")
+
+string.addEventListener("mousemove",(dets) => {
+  console.log("mouse entered")
+    finalPath = `M 200 500 Q 850 ${dets.y} 1700 500`
+    //console.log(initialPath)
+    gsap.to("svg path", {
+    attr: {
+      d: finalPath
+    }
+  })
+   
+})
+
+string.addEventListener("mouseleave",() => {
+  console.log("mouse leaves")
+  gsap.to("svg path",{
+    attr:{
+      d: initialPath
+    }
+  })
+
+})
