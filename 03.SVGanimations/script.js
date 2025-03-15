310470c50dd7f82fad9917b4aa137561ba7ca3fa
hemant <tyagi26326@gmail.com>
2025-03-15 20:59:37 +0530

    WIP

--- 03.SVGanimations/script.js
+++ 03.SVGanimations/script.js
@@ -1,21 +1,22 @@
 console.log("hello world");
 
-var initialPath = `M 200 500 Q 850 500 1700 500`
-var finalPath = `M 200 500 Q 850 500 1700 500`
+var initialPath = `M 200 200 Q 850 190 1700 200`
+var finalPath = `M 200 200 Q 850 100 1700 200`
 
 var string = document.querySelector(".string")
 
 string.addEventListener("mousemove",(dets) => {
   console.log("mouse entered")
-    finalPath = `M 200 500 Q 850 ${dets.y} 1700 500`
-    //console.log(initialPath)
-    gsap.to("svg path", {
+  finalPath = `M 200 200 Q ${dets.x} ${dets.y} 1700 200`
+  //console.log(initialPath)
+  gsap.to("svg path", {
     attr: {
       d: finalPath,
     },
-    duration:0.2
+    duration:0.2,
+    ease:"power3.out"
   })
-   
+
 })
 
 string.addEventListener("mouseleave",() => {
@@ -23,7 +24,9 @@ string.addEventListener("mouseleave",() => {
   gsap.to("svg path",{
     attr:{
       d: initialPath
-    }
+    },
+      duration:1.5,
+      ease:"elastic.out(1,0.2)"
   })
 
 })
